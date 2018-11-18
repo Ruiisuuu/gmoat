a871bbcc9dc992876da0754b8ecf91ee40bb7c87
Louis Mollick <louismollick@gmail.com>
2018-11-18 17:27:52 -0500

    renewed code

--- darkflow/skrub.py
+++ darkflow/skrub.py
@@ -4,83 +4,53 @@ import numpy as np
 import pandas as pd
 from pandas.io.json import json_normalize
 
-def players_to_frames(video_filename):
-    player1 = pd.DataFrame()
-    player2 = pd.DataFrame()
+def clip_to_data(video_filename, subject, options):
+    clipdata = pd.DataFrame()
+    tfnet = TFNet(options) #create darkflow
+
     cap = cv2.VideoCapture(video_filename)
     cfps = int(cap.get(cv2.CAP_PROP_FPS))
     dfps = 10
     totalframes = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
     video_length = totalframes/cfps
+    count = 0
+    success, image = cap.read()
+
     print("this will take " + str(totalframes/dfps) + " seconds...")
-    count = 0
-    success, image = cap.read()
-    while count % dfps == 0: #for each frame get json from darkflow, add to table
-        frame = np.array(image)
-        json = tfnet.return_predict(frame)
-        framedata = json_normalize(json)
-        framedata['x'] = (framedata['bottomright.x'] + framedata['topleft.x'])/2
-        framedata['y'] = (framedata['bottomright.y'] + framedata['topleft.y'])/2
-        print('logged players count: '+ str(count))
 
-        framedata = framedata.drop(['bottomright.x', 'bottomright.y', 'topleft.x', 'topleft.y'], 1)
-        framedata = framedata[framedata.label == 'person']
-        framedata1 = framedata[framedata.y >= 250]
-        framedata2 = framedata[framedata.y < 250]
-        framedata1 = framedata1.loc[framedata1.groupby('label')['confidence'].idxmax()]
-        framedata2 = framedata2.loc[framedata2.groupby('label')['confidence'].idxmax()]
-        framedata1['frame'] = count/dfps
-        framedata1['time'] = video_length*count/(dfps)**2
-        framedata1 = framedata1.set_index('time')
-        framedata2['frame'] = count/dfps
-        framedata2['time'] = video_length*count/(dfps)**2
-        framedata2 = framedata2.set_index('time')
-        player1 = player1.append(framedata1)
-        player2 = player2.append(framedata2)
+    while success: #for each frame get json from darkflow, add to table
+        if (count % dfps == 0):
+            frame = np.array(image)
+            json = tfnet.return_predict(frame) #darkflow
+            framedata = json_normalize(json)
+            framedata['x'] = (framedata['bottomright.x'] + framedata['topleft.x'])/2
+            framedata['y'] = (framedata['bottomright.y'] + framedata['topleft.y'])/2
+            framedata['frame'] = count/dfps
+            framedata['time'] = video_length*count/(dfps)**2
+            framedata = framedata.set_index(['frame','time'])
+
+            print('frame count: '+ str(int(count/dfps)))
+            framedata = framedata.drop(['bottomright.x', 'bottomright.y', 'topleft.x', 'topleft.y'], 1)
+
+            #take element witht the heighest probability to be our subject
+            if (subject == "person"):
+                framedata = framedata[framedata.label == 'person']
+                framedata = framedata[framedata.y >= 250]
+            elif(subject == "ball"):
+                framedata = framedata[framedata.label == 'sports ball']
+
+            framedata = framedata.loc[framedata1.groupby('label')['confidence'].idxmax()]
+            clipdata = clipdata.append(framedata)
         success, image = cap.read()
         count += 1
-    player1.to_csv("player1.csv")
-    player2.to_csv("player2.csv")
 
-    return
+    clipdata.to_csv(subject + ".csv")
+    return clipdata
 
-def ball_to_frames(video_filename):
-    ball = pd.DataFrame()
-
-    cap = cv2.VideoCapture(video_filename)
-    cfps = int(cap.get(cv2.CAP_PROP_FPS))
-    dfps = 10
-    totalframes = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
-    video_length = totalframes/cfps
-    print("this will take " + str(video_length) + " seconds...")
-    count = 0
-    success, image = cap.read()
-    while count % dfps == 0: #for each frame get json from darkflow, add to table
-        frame = np.array(image)
-        json = tfnet.return_predict(frame)
-        framedata = json_normalize(json)
-        framedata['x'] = (framedata['bottomright.x'] + framedata['topleft.x'])/2
-        framedata['y'] = (framedata['bottomright.y'] + framedata['topleft.y'])/2
-        print('logged balls count: '+ str(count))
-        framedata = framedata.drop(['bottomright.x', 'bottomright.y', 'topleft.x', 'topleft.y'], 1)
-        framedata = framedata[framedata.label == 'sports ball']
-        framedata = framedata.loc[framedata.groupby('label')['confidence'].idxmax()]
-        framedata['frame'] = count/dfps
-        framedata['time'] = video_length*count/(dfps)**2
-        framedata = framedata.set_index('time')
-        ball = ball.append(framedata)
-        success, image = cap.read()
-        count += 1
-    ball.to_csv("ball.csv")
-
-    return
-
-# get player coordinates
+# set darkflow options
 playeroptions = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.6}
-tfnet = TFNet(playeroptions)
-players_to_frames('test3.mp4')
-
-# get get ball coordinates
 balloptions = {"model": "cfg/yolo.cfg", "load": "bin/yolo.weights", "threshold": 0.001}
-tfnet = TFNet(balloptions)
-ball_to_frames('test3.mp4')
+
+#get coords
+player = clip_to_data('test3.mp4','person', playeroptions)
+ball = clip_to_data('test3.mp4','ball', balloptions)
